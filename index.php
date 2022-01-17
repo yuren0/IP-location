6c2b4172e42bdae877a40aeb7e0614e61194c606
yuren0 <45882709+yuren0@users.noreply.github.com>
2022-01-17 13:59:19 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,32 @@
 <?php
 //header("Content-type: text/html; charset=gb2312");
-    echo '<html>
-    <head>
-    <title>IP地理位置接口</title></head></html>';
+	echo '<html>
+	<head>
+	<title>IP地理位置接口</title></head></html>';
 if (!empty($_GET["ip"])){
-    $local=$_GET["ip"];
+	$local=$_GET["ip"];
 } else {
-    $local=$_SERVER["REMOTE_ADDR"];
+	$local=$_SERVER["REMOTE_ADDR"];
 }
 	$url="https://ip.chinaz.com/".$local;
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_HEADER, 1);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 1);   // ssl 访问核心参数
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+	curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 1);   // ssl 访问核心参数
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 	$response = curl_exec($ch);
 	$response = preg_replace('/\s+/', '', $response);
 	//file_put_contents('yuan.txt',$response);
 	curl_close($ch);
 	function get_em($input, $start, $end) {
-    $substr = substr($input, strlen($start)+strpos($input, $start),
-    (strlen($input) - strpos($input, $end))*(-1));
-    return $substr;
+	$substr = substr($input, strlen($start)+strpos($input, $start),
+	(strlen($input) - strpos($input, $end))*(-1));
+	return $substr;
 }
-    $string = $response;
-    $start = '0lh45"><em>';
-    $end = '</em><aclass';
-    echo 'IP：'.$local.'<br>位置：'.get_em($string, $start, $end);
+	$string = $response;
+	$start = '0lh45"><em>';
+	$end = '</em><aclass';
+	echo 'IP：'.$local.'<br>位置：'.get_em($string, $start, $end);
 
 ?>
